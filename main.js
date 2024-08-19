e7389c58fe05d9230c51444335ebd61f486b5539
Muhanned Noman <Muhanned.Noman@gmail.com>
2024-08-19 11:44:37 +0300

    feat: starter code

---
+++ main.js
@@ -0,0 +1,19 @@
+const input = [
+  [0, 1, 0, 0],
+  [0, 0, 1, 0],
+  [0, 1, 0, 1],
+  [1, 1, 0, 0],
+];
+
+const output = [
+  [1, 9, 2, 1],
+  [2, 3, 9, 2],
+  [3, 9, 4, 9],
+  [9, 9, 3, 1],
+]
+
+function mainSweeper(src){
+ return;
+}
+
+mineSweeper(input)
